50adbfdbef70356ba3bc0c59986bd6fe1977abbd
jesusos <ortegasanchez.jesusm@gmail.com>
2022-11-14 16:55:57 +0100

    sites added

--- apache1/consulta.php
+++ apache1/consulta.php
@@ -16,6 +16,6 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } else {
     echo "Se puede acceder desde apache1"."<br/>";
-    echo "Connected to MySQL server successfully!"."<br/>";
+    echo "Connected to MySQL server successfully!";
 }
 ?>
